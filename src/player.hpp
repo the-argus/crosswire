9b7712626d688caa3a02939a81d9050a52a243cf
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-01-09 12:55:08 -0600

    change formatting on player.hpp

--- src/player.hpp
+++ src/player.hpp
@@ -1,24 +1,20 @@
 #pragma once
-#include <raylib.h>
 #include "physics.hpp"
-#include "input.hpp"
-#include "player.hpp"
-#include "thelib/opt.hpp"
 #include <raylib.h>
-
 
 namespace cw {
-    struct player_t {
-        public:
-            void draw();
-            void update();
+struct player_t
+{
+  public:
+    void draw();
+    void update();
 
-            player_t();
+    player_t();
 
-        private:
-            static constexpr float speed = 100;
-            static constexpr float bounding_box_size = 10;
-            physics::raw_body_t body;
-            physics::raw_poly_shape_t shape;
-    };
-}
+  private:
+    static constexpr float speed = 100;
+    static constexpr float bounding_box_size = 10;
+    physics::raw_body_t body;
+    physics::raw_poly_shape_t shape;
+};
+} // namespace cw
