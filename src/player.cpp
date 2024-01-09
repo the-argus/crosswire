0ddb803c8081410e7f223b9a4fa64753c3b1694f
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-01-09 12:56:33 -0600

    format player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -1,32 +1,35 @@
-#include "physics.hpp"
-#include "input.hpp"
 #include "player.hpp"
+#include "physics.hpp"
 #include "thelib/rect.hpp"
 #include "thelib/shape.hpp"
 #include <raylib.h>
 
 namespace cw {
-player_t::player_t() 
-    : body(physics::create_body(game_id_e::Player, {
-            .type = lib::body_t::Type::DYNAMIC, 
-            .mass = 1.0f, 
-            .moment = INFINITY
-        })),
-        shape(physics::create_box_shape(body, {
-            .bounding = lib::rect_t({0, 0}, {bounding_box_size}),
-            .radius = 1
-
-        }))
+player_t::player_t()
+    : body(physics::create_body(game_id_e::Player,
+                                {
+                                    .type = lib::body_t::Type::DYNAMIC,
+                                    .mass = 1.0f,
+                                    .moment = INFINITY,
+                                })),
+      shape(physics::create_box_shape(
+          body, {
+                    .bounding = lib::rect_t({0, 0}, {bounding_box_size}),
+                    .radius = 1,
+                }))
 {
 }
 
-void player_t::draw() {
+void player_t::draw()
+{
     // draw sprite
-    //lib::vect_t pos = physics::get_body(body).position();
+    // lib::vect_t pos = physics::get_body(body).position();
 
-    //DrawRectangle(pos.x, pos.y, bounding_box_size, bounding_box_size, RED);
+    // DrawRectangle(pos.x, pos.y, bounding_box_size, bounding_box_size, RED);
 }
-void player_t::update() {
+
+void player_t::update()
+{
     lib::vect_t velocity(0);
 
     // check which movement key is pressed, set body velocity to that dir
@@ -36,7 +39,7 @@ void player_t::update() {
     } else if (IsKeyDown(KEY_RIGHT)) {
         velocity.x = 1;
     }
-    
+
     if (IsKeyDown(KEY_UP)) {
         velocity.y = -1;
     } else if (IsKeyDown(KEY_DOWN)) {
@@ -55,6 +58,5 @@ void player_t::update() {
     physics::get_body(body).set_velocity(velocity);
 
     // orient the body to the dir of the velocity
-
 }
-}
+} // namespace cw
