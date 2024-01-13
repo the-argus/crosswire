8576ce13fea31d18514e11520aa4ffbf9959d76a
Tom-Roman <tfr8811@rit.edu>
2024-01-13 18:11:38 -0500

    body->shape

--- src/player.cpp
+++ src/player.cpp
@@ -103,16 +103,16 @@ void player_t::collision_handler_static(cpArbiter *arb, cpSpace *space, cpDataPo
         return;
 
     // If colliding with a body whose ID is build_site and player presses a button and that build site is not attached to wire
-    physics::raw_body_t body_b = physics::get_handle_from_body(*lib::body_t::from_chipmunk(arb->body_b));
-    auto maybe_other_id = physics::get_id(body_b);
+    physics::raw_poly_shape_t shape_b = physics::get_handle_from_polygon_shape((lib::shape_t*) *arb->b);
+    auto maybe_other_id = physics::get_id(shape_b);
     if (maybe_other_id.has_value() &&
         maybe_other_id.value() == game_id_e::Build_Site && 
-        !((build_site_t*)(physics::get_user_data(body_b).value()))->get_state()
+        !((build_site_t*)(physics::get_user_data(shape_b).value()))->get_state()
     ) {
         // If player is not holding wire 
         if (!((player_t*)(userData))->holding_wire) {
             // attach wire to that build site
-            if (auto data = physics::get_user_data(body_b)) {
+            if (auto data = physics::get_user_data(shape_b)) {
                 ((player_t*)(userData))->wire.start_wire((*(build_site_t*)(data.value())));
                 ((player_t*)(userData))->holding_wire = true;
             }
@@ -121,7 +121,7 @@ void player_t::collision_handler_static(cpArbiter *arb, cpSpace *space, cpDataPo
         // If player is holding wire and the wire is not tangled
         else if (((player_t*)(userData))->wire.check_wire_validity()) {
             // both build sites the wire connects to shall be marked as complete
-            ((player_t*)(userData))->wire.end_wire((*(build_site_t*)(physics::get_user_data(body_b).value())));
+            ((player_t*)(userData))->wire.end_wire((*(build_site_t*)(physics::get_user_data(shape_b).value())));
             ((player_t*)(userData))->holding_wire = false;
         } 
     }
